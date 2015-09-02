6cc7ef60e49343250879caf4ae6832171e2981d5
ProDota2 <igorzv013@gmail.com>
2015-09-02 18:23:23 +0300

    v2.0.0

--- interpreter.cpp
+++ interpreter.cpp
@@ -4,162 +4,101 @@
 #include <string>
 #include <fstream>
 #include <map>
+#include "library.h"
 using namespace std;
+
 ifstream fin;
 map<string, int>var;
 
-void error(int x){
-	if (x == 0){
-		cout << "ERROR\n";
-		cout << "division by zero\n";
-	}
-	if (x == 1){
-		cout << "ERROR\n";
-		cout << "trouble in a cycle\n";
+vector < vector < string > > code;
+vector < vector < short > > type;
+//1-int
+//2-string
+//3-operator
+void parse(){
+	string s;
+	int j = 0;
+	while (getline(fin, s)){
+		code.push_back(vector<string>());
+		type.push_back(vector<short>());
+		for (int i = 0; i < (int)s.length(); i++){
+			string add;
+			if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z'){
+				while (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z' && i<s.length())
+					add += s[i], i++;
+				code[j].push_back(add);
+				type[j].push_back(2);
+			}
+			add = "";
+			if (s[i] >= '0' && s[i] <= '9'){
+				while (s[i] >= '0' && s[i] <= '9' && i < s.length())
+					add += s[i], i++;
+				code[j].push_back(add);
+				type[j].push_back(1);
+			}
+			add = "";
+			if (s[i] == '='){
+				while (s[i] == '=' && i < s.length())
+					add += s[i], i++;
+				code[j].push_back(add);
+				type[j].push_back(3);
+			}
+			add = s[i];
+			if (s[i] == '+')
+				code[j].push_back(add), type[j].push_back(3);
+			if (s[i] == '-')
+				code[j].push_back(add), type[j].push_back(3);
+			if (s[i] == '*')
+				code[j].push_back(add), type[j].push_back(3);
+			if (s[i] == '/')
+				code[j].push_back(add), type[j].push_back(3);
+		}
+		j++;
 	}
 }
-int obr(int now,int x,char oper){
-	if (oper == '+')
-		now += x;
-	if (oper == '-')
-		now -= x;
-	if (oper == '*')
-		now *= x;
-	if (x == 0 && oper == '/'){
-		error(0);
-		system("pause");
-		exit(0);
-	}
-	if (oper == '/')
-		now /= x;
-	return now;
-}
-int prs(string s, int x){
-	int ans = 0;
+int calc(int line, int from){
 	char oper = '+';
-	for (int i = x; i<(int)s.length(); i++){
-		int add = 0;
-		string s_add = "";
-		bool is_digit = true;
-		if (s[i] >= '0' && s[i] <= '9'){
-			while (s[i] >= '0' && s[i] <= '9' && i < (int)s.length())
-				add *= 10, add += s[i] - '0', i++;
-			ans = obr(ans, (is_digit ? add : var[s_add]), oper);
-		}
-		else if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z'){
-			is_digit = false;
-			while (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z' && i < (int)s.length())
-				s_add += s[i], i++;
-			ans = obr(ans, (is_digit ? add : var[s_add]), oper);
-		}
-		if (s[i] == '+')
-			oper = s[i];
-		if (s[i] == '-')
-			oper = s[i];
-		if (s[i] == '*')
-			oper = s[i];
-		if (s[i] == '/')
-			oper = s[i];
+	int ans = 0;
+	for (int i = from; i < code[line].size(); i++){
+		if (type[line][i] == 3)
+			oper = code[line][i][0];
+		else
+		if (type[line][i] == 2)
+			ans = obr(ans, var[code[line][i]], oper);
+		else
+			ans = obr(ans, ToInt(code[line][i]), oper);
 	}
 	return ans;
 }
-void read(string s, int x){
-	string name;
-	for (int i = x; i < (int)s.length();i++)
-	if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z'){
-		while (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z' && i < (int)s.length())
-			name += s[i], i++;
-	}
-	cin >> var[name];
+void print(int line){
+	cout << calc(line, 1);
 }
-void cycle(string s, int x){
-	int from = -1, to = -1;
-	string id = "none";
-	for (int i = x; i<(int)s.length(); i++){
-		if (s[i] >= '0' && s[i] <= '9' && from == -1){
-			from = 0;
-			while (s[i] >= '0' && s[i] <= '9' && i<(int)s.length())
-				from *= 10, from += s[i] - '0', i++;
-		}
-		if (s[i] >= '0' && s[i] <= '9' && to == -1){
-			to = 0;
-			while (s[i] >= '0' && s[i] <= '9' && i<(int)s.length())
-				to *= 10, to += s[i] - '0', i++;
-		}
-		if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z'){
-			id = "";
-			while (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z' && i < (int)s.length())
-				id += s[i], i++;
-		}
-	}
-	if (from == -1 || to == -1){
-		error(1);
-		return;
-	}
-	var[id] = from;
-	string str;
-	vector<string>cyc;
-	while (getline(fin, str)){
-		if (str == "##")
-			break;
-		cyc.push_back(str);
-	}
-	for (int t = from; t < to; t++){
-		for (int j = 0; j < (int)cyc.size(); j++){
-			string name;
-			int ii;
-			for (ii = 0; ii < (int)cyc[j].length(); ii++){
-				if (cyc[j][ii] >= 'a' && cyc[j][ii] <= 'z' || cyc[j][ii] >= 'A' && cyc[j][ii] <= 'Z'){
-					while (cyc[j][ii] >= 'a' && cyc[j][ii] <= 'z' || cyc[j][ii] >= 'A' && cyc[j][ii] <= 'Z' && ii < (int)cyc[j].length())
-						name += cyc[j][ii], ii++;
-					break;
-				}
-			}
-			if (name == "print"){
-				cout << prs(cyc[j], ii);
-				continue;
-			}
-			if (name == "println"){
-				cout << prs(cyc[j], ii) << endl;
-				continue;
-			}
-			if (name == "read"){
-				read(cyc[j],ii);
-				continue;
-			}
-			var[name] = prs(cyc[j], ii);
-		}
-		var[id]++;
+void println(int line){
+	cout << calc(line, 1) << endl;
+}
+void read(int line){
+	for (int i = 1; i < code[line].size(); i++){
+		if (code[line][i] == "for" || code[line][i] == "if")
+			error(2, line);
+		cin >> var[code[line][i]];
 	}
 }
-void interpret(){
-	string s;
-	while (getline(fin, s)){
-		string name;
-		int i;
-		for (i = 0; i < (int)s.length(); i++)
-		if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z'){
-			while (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z' && i < (int)s.length())
-				name += s[i], i++;
-			break;
+void doit(int line){
+	if (code[line][0] == "read")
+		read(line);
+	if (code[line][0] == "print")
+		print(line);
+	if (code[line][0] == "println")
+		println(line);
+	if (type[line][1] == 3){
+		var[code[line][0]] = calc(line, 2);
+	}
+}
+void interpret2(){
+	for (int i = 0; i<code.size(); i++){
+		if (code[i][0] != "for" && code[i][0] != "if"){
+			doit(i);
 		}
-		if (name == "print"){
-			cout << prs(s, i);
-			continue;
-		}
-		if (name == "println"){
-			cout << prs(s, i) << endl;
-			continue;
-		}
-		if (name == "for"){
-			cycle(s, i);
-			continue;
-		}
-		if (name == "read"){
-			read(s, i);
-			continue;
-		}
-		var[name] = prs(s, i);
 	}
 }
 int main(int argc, char* argv[]){
@@ -167,10 +106,14 @@ int main(int argc, char* argv[]){
 	freopen("output.txt", "w", stdout);
 	if (argc > 1){
 		fin = ifstream(argv[argc - 1]);
-		interpret();
+		parse();
+		interpret2();
 	}
 	else{
-		cout << "Sorry but it is not an .myl file";
+		fin = ifstream("ppp.myl");
+		parse();
+		interpret2();
+		//interpret();
 	}
 	return 0;
 }
